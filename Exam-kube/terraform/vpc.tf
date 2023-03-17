1a33d7262c187861767e7cf7fd46b2fe2acc0e1f
Susan Adebambo Bamgbade <103441089+feistymilady01@users.noreply.github.com>
2023-03-18 00:09:22 +0100

    task 1

--- Exam-kube/terraform/vpc.tf
+++ Exam-kube/terraform/vpc.tf
@@ -1,28 +1,28 @@
 data "aws_availability_zones" "azs" {}
-module "myapp-vpc" {
-  source          = "terraform-aws-modules/vpc/aws"
-  version         = "3.19.0"
-  name            = "myapp-vpc"
-  cidr            = var.vpc_cidr_block
+module "vpc-micro-app" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 3.0"
+  name = "micro-app"
+  cidr = var.vpc_cidr_block
   private_subnets = var.private_subnet_cidr_blocks
-  public_subnets  = var.public_subnet_cidr_blocks
-  azs             = data.aws_availability_zones.azs.names
+  public_subnets = var.public_subnet_cidr_blocks
+  azs = data.aws_availablity_zones.azs.names
 
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
 
   tags = {
-    "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
-  }
-
+    "kubernates.io/cluster/eks-cluster-micro-app" = "shared"
+  
   public_subnet_tags = {
-    "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
-    "kubernetes.io/role/elb"                  = 1
+    "kubernates.io/cluster/eks-cluster-micro-app" = "shared"
+    "kubernates.io/role/elb" = 1
   }
 
   private_subnet_tags = {
-    "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
-    "kubernetes.io/role/internal-elb"         = 1
+    "kubernates.io/cluster/eks-cluster-micro-app" = "shared"
+    "kubernates.io/role/elb" = 1
+  }
   }
 }
