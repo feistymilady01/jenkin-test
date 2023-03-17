c3b9acc8e2b1019c499bae7924dd2ca31393e215
Susan Adebambo Bamgbade <103441089+feistymilady01@users.noreply.github.com>
2023-03-18 00:13:03 +0100

    TASK 2

--- Exam-kube/terraform/eks-cluster.tf
+++ Exam-kube/terraform/eks-cluster.tf
@@ -1,7 +1,7 @@
-module "eks" {
+module "eks-cluster" {
     source  = "terraform-aws-modules/eks/aws"
     version = "~> 19.0"
-    cluster_name = "myapp-eks-cluster"
+    cluster_name = "eks-cluster-micro-app"
     cluster_version = "1.24"
 
     cluster_endpoint_public_access  = true
@@ -11,7 +11,7 @@ module "eks" {
 
     tags = {
         environment = "development"
-        application = "myapp"
+        application = "-micro-app"
     }
 
     eks_managed_node_groups = {
