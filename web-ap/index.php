8d9f6d0061052402d26f4cafb734fe28afb0a1d8
susan <pricelessblinks@gmail.com>
2023-03-18 00:06:05 +0100

    exam

--- web-ap/index.php
+++ web-ap/index.php
@@ -1,9 +1,26 @@
 <?php
-		if ($_POST) {
-			$color = $_POST['color'];
-			$file = fopen("index.html", "a");
-			fwrite($file, "$color\n");
-			fclose($file);
-			echo "<p>Thanks for voting!</p>";
-		}
-	?>
+// Establish a connection to the MySQL database
+$servername = "localhost";
+$username = "yourusername";
+$password = "yourpassword";
+$dbname = "yourdatabase";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check if the form was submitted
+if ($_POST) {
+    // Get the color value from the form
+    $color = $_POST['color'];
+
+    // Insert the color value into the database
+    $sql = "INSERT INTO color_votes (color) VALUES ('$color')";
+    if (mysqli_query($conn, $sql)) {
+        echo "<p>Thanks for voting!</p>";
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+}
+
+// Close the MySQL connection
+mysqli_close($conn);
+?>
