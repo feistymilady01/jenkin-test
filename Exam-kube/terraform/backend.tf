71b31b49fa7b2dad4664ffd79c98a2bde49d40c1
Susan Adebambo Bamgbade <103441089+feistymilady01@users.noreply.github.com>
2023-03-18 00:13:53 +0100

    task 3

--- Exam-kube/terraform/backend.tf
+++ Exam-kube/terraform/backend.tf
@@ -1,5 +1,7 @@
-terraform {
-  backend "local" {
-    path = "terraform.tfstate"
-  }
+terraform{
+    backend "s3" {
+        bucket = "micro-service-app-15-03-2023"
+        region = "eu-west-2"
+        key ="eks-server/terraform.tfstate"
+    }
 }
